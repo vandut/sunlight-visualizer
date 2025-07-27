c67540b8b896acb294fb350ca614c0115c591eb8
vandut <konrad.bielak@gmail.com>
2025-07-27 21:25:23 +0200

    Update base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+      base: '/sunlight-visualizer/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
