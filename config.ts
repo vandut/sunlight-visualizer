d48b5a484a12ccc4e35901218d8cdeb0c1fd3e97
vandut <konrad.bielak@gmail.com>
2025-07-27 21:59:36 +0200

    update client id

--- config.ts
+++ config.ts
@@ -6,4 +6,4 @@
 // and Authorized JavaScript origins for the Client ID) in your Google Cloud project.
 
 export const GOOGLE_API_KEY = 'AIzaSyC5k7-HBubG4e-C-pw-1uv7_leMoJDCtZ0';
-export const GOOGLE_CLIENT_ID = '31266781685-38uq2rv4msggrt83me42sqblk7f6d7si.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '31266781685-vdrp5misp801p8kqs4bb1v243mupvvh9.apps.googleusercontent.com';
